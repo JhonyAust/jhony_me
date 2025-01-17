e6567cd1dbeba58f766adb6d1923429965a75de3
Jhony <jhonyaustcse37@gmail.com>
2025-01-17 12:21:49 +0600

    Update Socials Link

--- components/Socials.js
+++ components/Socials.js
@@ -5,25 +5,26 @@ import {
   RiFacebookLine,
   RiLinkedinLine,
   RiPinterestLine,
+  RiGithubLine,
 } from 'react-icons/ri'
 
 const Socials = () => {
   return (
     <div className='flex items-center gap-x-5 text-lg z-50'>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.youtube.com/@rayhanjhony75'} className='hover:text-accent transition-all duration-300'>
         <RiYoutubeLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiInstagramLine />
+      <Link href={'https://github.com/JhonyAust'} className='hover:text-accent transition-all duration-300'>
+        <RiGithubLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.facebook.com/jhonyaustcse37'} className='hover:text-accent transition-all duration-300'>
         <RiFacebookLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/rayhan-jhony-baa53a1a9/'} className='hover:text-accent transition-all duration-300'>
         <RiLinkedinLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiPinterestLine />
+      <Link href={'https://www.facebook.com/jhonyaustcse37'} className='hover:text-accent transition-all duration-300'>
+        <RiInstagramLine />
       </Link>
     </div>
   );
